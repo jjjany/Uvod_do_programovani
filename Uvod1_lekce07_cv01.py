6cba72762b844dfcbf108f7e1115cf0d436a7a76
Jana Jameba Jany <jjjany111@gmail.com>
2023-04-19 15:21:24 +0200

    Úložka 1, lekce 7

---
+++ Uvod1_lekce07_cv01.py
@@ -0,0 +1,9 @@
+# Řetězce jako sekvence
+
+#     Uložte do proměnné jmeno svoje celé jméno a nechte vypsat jeho třetí, pátý a sedmý znak. Vyzkoušejte, co se stane, když budete chtít znak na pozici, která překračuje délku řetězce.
+
+jmeno = "Jana Jaméba Jany"
+print(jmeno[2], jmeno[4], jmeno[6])
+
+# print(jmeno[20])
+# => IndexError: string index out of range
